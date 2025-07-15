aeeab1c17bfe5e7fbfa0d9bf8d901db086167ca3
UnripePlum <khjalk8625@kaist.ac.kr>
2025-07-15 02:50:35 +0000

    feat; stock list

--- app/api/endpoints/stock.py
+++ app/api/endpoints/stock.py
@@ -10,3 +10,8 @@ router = APIRouter()
 @router.get("/list", response_model=List[str])
 async def stock_list(db: AsyncSession = Depends(get_db)):
     return await crud_stock.get_stock_list(db=db)
+
+@router.post("/init")
+async def initialize_stocks(db: AsyncSession = Depends(get_db)):
+    await crud_stock.insert_initial_stocks(db)
+    return {"message": "Stocks inserted"}
--- app/crud/crud_stock.py
+++ app/crud/crud_stock.py
@@ -2,7 +2,19 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from app.database import models
 import time
+
 async def get_stock_list(db: AsyncSession):
     query = select(models.Stocks)
     result = await db.execute(query)
     return result.scalars().all()
+
+async def insert_initial_stocks(db: AsyncSession):
+    stock_entries = [
+        models.Stocks(symbol="HWG", name="황광호날두회사", sector="Technology", industry="Consumer Electronics"),
+        models.Stocks(symbol="GOOGO", name="구골", sector="Technology", industry="Internet Services"),
+        models.Stocks(symbol="TGLA", name="테킬라", sector="Automotive", industry="Electric Vehicles"),
+        models.Stocks(symbol="BTC", name="비트컴퍼니", sector="Coin", industry="Cryptocurrency"),
+    ]
+
+    db.add_all(stock_entries)
+    await db.commit()
