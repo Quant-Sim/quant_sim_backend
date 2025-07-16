b4dd4b98271c0502e861cd115a83452089042ec1
Mac M3 <hanjunkim@Macui-MacBookAir.local>
2025-07-16 16:16:24 +0900

    fix; colors

--- app/crud/crud_order.py
+++ app/crud/crud_order.py
@@ -14,11 +14,6 @@ colors = [
     'bg-red-100',
     'bg-yellow-100',
     'bg-purple-100',
-    'bg-pink-100',
-    'bg-orange-100',
-    'bg-teal-100',
-    'bg-indigo-100',
-    'bg-gray-100',
 ]
 
 
